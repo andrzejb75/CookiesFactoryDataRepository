5d3949bb7c4e28f22b5fb1e7430ab06f0037af5c
Andrzej Butrym <andrzej.butrym@kec.kingspan.net>
2024-07-21 19:23:12 +1000

    Add project files.

---
+++ CookiesFactoryAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CookiesFactoryAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
